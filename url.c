5111599eb6d607f3ab3cf999c0a50419d9a7867d
Scarletts <scarlett@xavin.net>
2014-12-16 18:41:41 -0600

    remove unsafe RNG seeding

--- url.c
+++ url.c
@@ -257,36 +257,6 @@ free_ssl_ctx()
     ssl_accept_this_site(NULL);
 }
 
-#if SSLEAY_VERSION_NUMBER >= 0x00905100
-#include <openssl/rand.h>
-static void
-init_PRNG()
-{
-    char buffer[256];
-    const char *file;
-    long l;
-    if (RAND_status())
-	return;
-    if ((file = RAND_file_name(buffer, sizeof(buffer)))) {
-#ifdef USE_EGD
-	if (RAND_egd(file) > 0)
-	    return;
-#endif
-	RAND_load_file(file, -1);
-    }
-    if (RAND_status())
-	goto seeded;
-    srand48((long)time(NULL));
-    while (!RAND_status()) {
-	l = lrand48();
-	RAND_seed((unsigned char *)&l, sizeof(long));
-    }
-  seeded:
-    if (file)
-	RAND_write_file(file);
-}
-#endif				/* SSLEAY_VERSION_NUMBER >= 0x00905100 */
-
 static SSL *
 openSSLHandle(int sock, char *hostname, char **p_cert)
 {
