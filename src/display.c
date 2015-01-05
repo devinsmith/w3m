1c828b430e9200922db10f8559262734106e0566
Scarletts <scarlett@xavin.net>
2015-01-05 16:42:11 -0600

    include math.h for floor() and log()

--- src/display.c
+++ src/display.c
@@ -1,5 +1,6 @@
 /* $Id: display.c,v 1.71 2010/07/18 14:10:09 htrb Exp $ */
 #include <signal.h>
+#include <math.h>
 #include "fm.h"
 
 /* *INDENT-OFF* */
--- src/file.c
+++ src/file.c
@@ -14,6 +14,7 @@
 #include <utime.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 /* foo */
 
 #include "html.h"
