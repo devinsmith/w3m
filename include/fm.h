658f6b3192d608144f0d19de8df3d65432e1564e
Scarlett <scarlett@sakaki.entering.space>
2015-04-16 18:47:34 +0100

    status is of type RTstatus

--- include/fm.h
+++ include/fm.h
@@ -539,6 +539,26 @@ typedef struct {
 	short bottom_margin;
 } Breakpoint;
 
+/* status when reading tokens */
+enum RTstatus {
+	R_ST_NORMAL	= 0,	/* normal */
+	R_ST_TAG0	= 1,	/* within tag, just after < */
+	R_ST_TAG	= 2,	/* within tag */
+	R_ST_QUOTE	= 3,	/* within single quote */
+	R_ST_DQUOTE	= 4,	/* within double quote */
+	R_ST_EQL	= 5,	/* = */
+	R_ST_AMP	= 6,	/* within ampersand quote */
+	R_ST_EOL	= 7,	/* end of file */
+	R_ST_CMNT1	= 8,	/* <!  */
+	R_ST_CMNT2	= 9,	/* <!- */
+	R_ST_CMNT	= 10,	/* within comment */
+	R_ST_NCMNT1	= 11,	/* comment - */
+	R_ST_NCMNT2	= 12,	/* comment -- */
+	R_ST_NCMNT3	= 13,	/* comment -- space */
+	R_ST_IRRTAG	= 14,	/* within irregular tag */
+	R_ST_VALUE	= 15	/* within tag attribule value */
+};
+
 struct readbuffer {
 	Str line;
 	Lineprop cprop;
@@ -547,7 +567,7 @@ struct readbuffer {
 	long flag;
 	long flag_stack[RB_STACK_SIZE];
 	int flag_sp;
-	int status;
+	enum RTstatus status;
 	unsigned char end_tag;
 	short table_level;
 	short nobr_level;
@@ -610,26 +630,6 @@ struct readbuffer {
   if ((obuf)->flag_sp > 0) \
    RB_SET_ALIGN(obuf,(obuf)->flag_stack[--(obuf)->flag_sp]); \
 }
-
-/* status when reading tokens */
-enum RTstatus {
-	R_ST_NORMAL	= 0,	/* normal */
-	R_ST_TAG0	= 1,	/* within tag, just after < */
-	R_ST_TAG	= 2,	/* within tag */
-	R_ST_QUOTE	= 3,	/* within single quote */
-	R_ST_DQUOTE	= 4,	/* within double quote */
-	R_ST_EQL	= 5,	/* = */
-	R_ST_AMP	= 6,	/* within ampersand quote */
-	R_ST_EOL	= 7,	/* end of file */
-	R_ST_CMNT1	= 8,	/* <!  */
-	R_ST_CMNT2	= 9,	/* <!- */
-	R_ST_CMNT	= 10,	/* within comment */
-	R_ST_NCMNT1	= 11,	/* comment - */
-	R_ST_NCMNT2	= 12,	/* comment -- */
-	R_ST_NCMNT3	= 13,	/* comment -- space */
-	R_ST_IRRTAG	= 14,	/* within irregular tag */
-	R_ST_VALUE	= 15	/* within tag attribule value */
-};
 
 #define ST_IS_REAL_TAG(s)   ((s)==R_ST_TAG||(s)==R_ST_TAG0||(s)==R_ST_EQL||(s)==R_ST_VALUE)
 
