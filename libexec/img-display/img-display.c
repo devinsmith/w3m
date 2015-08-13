f18a7239b62ebf07b24782e02f923855c31cd023
Scarletts <scarlett@entering.space>
2015-08-13 01:39:55 +0100

    another simple use of tame

--- libexec/img-display/img-display.c
+++ libexec/img-display/img-display.c
@@ -1,4 +1,5 @@
 /* $Id: w3mimgdisplay.c,v 1.19 2010/12/21 10:13:55 htrb Exp $ */
+#include <sys/tame.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -30,6 +31,8 @@ main(int argc, char **argv)
 {
 	int len;
 	char buf[1024 + 128];
+
+	tame(TAME_STDIO);
 
 	GetOption(argc, argv);
 	if (!defined_debug)
