575c26c1f1eb37a1e2cfea5262b855859d520a48
Devin Smith <devin@devinsmith.net>
2021-05-06 17:46:55 -0700

    Namespace file_handle -> w3s_file_handle
    Linux already defines file_handle for some system calls, so to avoid a
    collision we rename the existing file_handle struct to w3s_file_handle.

--- include/istream.h
+++ include/istream.h
@@ -15,7 +15,7 @@ struct stream_buffer {
 
 typedef struct stream_buffer *StreamBuffer;
 
-struct file_handle {
+struct w3s_file_handle {
 	FILE *f;
 	void (*close) ();
 };
@@ -46,7 +46,7 @@ struct base_stream {
 
 struct file_stream {
 	struct stream_buffer stream;
-	struct file_handle *handle;
+	struct w3s_file_handle *handle;
 	char type;
 	char iseos;
 	int (*read) ();
